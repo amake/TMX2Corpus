c1e290b8b84e1e593e7acc854df4c5be11a52ad8
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-05-11 18:01:38 +0900

    Refactor tokenizing, remove XML-like tags

--- tokenizer.py
+++ tokenizer.py
@@ -11,107 +11,49 @@ import re
 import tinysegmenter
 
 BOUNDARY_REGEX = re.compile(r'\b|\Z')
+TAG_REGEX = re.compile(r'<[^>]+>')
 
 
 class Tokenizer(object):
     def __init__(self, lang):
         self.lang = lang
+
     def tokenize(self, text):
-        '''Override this to return tokenized text.'''
-        return text
+        text = TAG_REGEX.sub('', text)
+        tokens = self._tokenize(text)
+        if '://' in text or '@' in text:
+            tokens = glom_urls(tokens)
+        tokens = [tok for tok in tokens if not tok.strip() == '']
+        return ' '.join(tokens)
+
+    def _tokenize(self, text):
+        '''Override this to implement the actual tokenization: Take string,
+return list of tokens.'''
+        raise NotImplementedError
 
 
 class PyJaTokenizer(Tokenizer):
     def __init__(self):
         super(PyJaTokenizer, self).__init__('ja')
         
-    def tokenize(self, text):
-        tokenizer = tinysegmenter.TinySegmenter()
-        tokens = tokenizer.tokenize(text)
-        if '<' in text:
-            tokens = glom_tags(tokens)
-        if '://' in text or '@' in text:
-            tokens = glom_urls(tokens)
-        return ' '.join(tokens)
+    def _tokenize(self, text):
+        return tinysegmenter.TinySegmenter().tokenize(text)
 
 
 class PyEnTokenizer(Tokenizer):
     def __init__(self):
         super(PyEnTokenizer, self).__init__('en')
 
-    def tokenize(self, text):
+    def _tokenize(self, text):
         tokens = []
         i = 0
         for m in BOUNDARY_REGEX.finditer(text):
             tokens.append(text[i:m.start()])
             i = m.end()
-        return ' '.join([tok for tok in tokens if not tok.strip() == ''])
+        return tokens
 
     
 DEFAULT_TOKENIZER = PyEnTokenizer()
-
-
-def glom_tags(tokens):
-    result = []
-    in_tag = False
-    tag = None
-    #print 'Before:', str(tokens)
-    tokens.reverse()
-    while len(tokens):
-        tok = tokens.pop()
-        if in_tag:
-            if tok == '>':
-                result.append(tag + tok)
-                in_tag = False
-            elif tok.startswith('>'):
-                result.append(tag + '>')
-                in_tag = False
-                tokens.append(tok[1:])
-            elif tok.startswith('/>'):
-                result.append(tag + '/>')
-                in_tag = False
-                tokens.append(tok[2:])
-            else:
-                tag += tok
-        else:
-            if tok in ('<', '</'):
-                tag = tok
-                in_tag = True
-            elif tok.endswith('<'):
-                tag = '<'
-                in_tag = True
-                result.append(tok[:-1])
-            elif tok.endswith('</'):
-                tag = '</'
-                in_tag = True
-                result.append(tok[:-2])
-            else:
-                result.append(tok)
-    #print 'After:', str(result)
-    return glom_multitags(result)
-
-
-def is_tag(token):
-    return len(token) > 2 and token[0] == '<' and token[-1] == '>'
-
-
-def glom_multitags(tokens):
-    result = []
-    #print 'Before:', str(tokens)
-    tokens.reverse()
-    while len(tokens):
-        tok = tokens.pop()
-        if is_tag(tok) and len(tokens):
-            next_tok = tokens.pop()
-            if is_tag(next_tok):
-                tokens.append(tok + next_tok)
-            else:
-                result.append(tok)
-                result.append(next_tok)
-        else:
-            result.append(tok)
-    #print 'After:', str(result)
-    return result
 
 
 def glom_urls(tokens):
